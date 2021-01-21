1a7ccd57a06c2d189b37650c5eb5d668c9486199
Yann Rannou <yann.rannou777@gmail.com>
2021-01-21 09:49:12 +0100

    scrapper 1

--- lib/test.rb
+++ lib/test.rb
@@ -1,4 +1,25 @@
 require 'nokogiri'
 require 'open-uri'
 
-page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+def scrapper
+    page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
+  end
+
+
+
+def crypto(page)
+    array_name = []
+    array_value = []
+    name_of_crypto = page.xpath('//*[@id="__next"]//tr//td[2]')
+    value_of_crypto = page.xpath('//*[@id="__next"]//tr//td[5]')
+
+    name_of_crypto.each do |name|
+      array_name << name.text
+    end
+    value_of_crypto.each do |value|
+      array_value << value.text
+    end
+mon_hash = Hash[array_name.zip array_value.map]
+puts mon_hash
+end
+crypto(scrapper)
